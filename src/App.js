932e05169d657a8a556f748c5f578aa9fe57e908
EddieA <avendanoh351@gmail.com>
2020-12-22 13:20:12 -0800

    added local storage using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react' 
+import React, {useState,useEffect} from 'react' 
 import TodoForm from "./TodoForm"
 import TodoList from './TodoList'
 
@@ -6,18 +6,27 @@ import TodoList from './TodoList'
 function App() {
   const [todo, setTodo] = useState([]);
 
-function insertTodo(t) {
-  setTodo([t,...todo]);
-}
+  useEffect(() => {
+    const todo = JSON.parse(localStorage.getItem('todo'));
+    if (todo){setTodo(todo)}
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('todo',JSON.stringify(todo));
+  }, [todo]);
+
+  function insertTodo(t) {
+    setTodo([t,...todo]);
+  }
 
 
 
-  return (
-    <div>
-    <div className="App"> Hello World This is a todo app</div>
-    <TodoForm insertTodo ={insertTodo}/>
-    <TodoList todo={todo}/>
-    </div>
-  );
+    return (
+      <div>
+      <div className="App"> Hello World This is a todo app</div>
+      <TodoForm insertTodo ={insertTodo}/>
+      <TodoList todo={todo}/>
+      </div>
+    );
 }
 export default App;
