43424aea569832a76687c2447220d5b16060e7cd
EddieA <avendanoh351@gmail.com>
2020-12-29 15:39:14 -0800

    synthwave Buttons

--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,54 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default function Todo({todo,removeTodo,toggleIsComplete}) {
-    function handleRemClick(){removeTodo(todo.id)}
+// create synthwave button, complete, line-through using the synthw
 
-    function handleCompleteToggle(){ toggleIsComplete(todo.id)}
-    
+
+const Button = styled.button`
+  background: #ff34b3;
+  background-image: -webkit-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -moz-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -ms-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -o-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: linear-gradient(to bottom, #ff34b3, #2980b9);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Courier New;
+  color: #ffffff;
+  font-size: 12px;
+  padding: 5px 10px 5px 10px;
+  text-decoration: none;
+
+  &:hover {
+    background: #3cb0fd;
+    background-image: -webkit-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -moz-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -ms-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -o-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: linear-gradient(to bottom, #3cb0fd, #ff34b3);
+    text-decoration: none;
+}`;
+
+
+export default function Todo({ todo, removeTodo, toggleIsComplete }) {
+    function handleRemClick() { removeTodo(todo.id) }
+
+    function handleCompleteToggle() { toggleIsComplete(todo.id) }
+
     return (
-        <div style={{display: "flex"}}>
-            <input type="checkbox" onClick={handleCompleteToggle}/>
+        <div style={{ display: "flex" }}>
+            <input type="checkbox" onClick={handleCompleteToggle} />
             <li
                 style={{
                     textDecoration: todo.isComplete ? "line-through" : null
                 }}
 
-                >
-                    {todo.title}
+            >
+                {todo.title}
 
-                    </li>
-                    <button onClick={handleRemClick}>Remove</button> 
-            </div>
+            </li>
+            <Button onClick={handleRemClick}>Remove</Button>
+        </div>
     );
 }
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,6 +1,36 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import styled from 'styled-components'
+
+const Input = styled.input`
+  background: #ff34b3;
+  background-image: -webkit-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -moz-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -ms-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: -o-linear-gradient(top, #ff34b3, #2980b9);
+  background-image: linear-gradient(to bottom, #ff34b3, #2980b9);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  font-family: Courier New;
+  color: #ffffff;
+  font-size: 12px;
+  padding: 5px 10px 5px 10px;
+  text-decoration: none;
+
+  &:hover {
+    background: #3cb0fd;
+    background-image: -webkit-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -moz-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -ms-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: -o-linear-gradient(top, #3cb0fd, #ff34b3);
+    background-image: linear-gradient(to bottom, #3cb0fd, #ff34b3);
+    text-decoration: none;
+}
+`;
+
+
+
 export default function TodoForm({ insertTodo }) {
 
     const [currTodo, setcurrTodo] = useState({
@@ -29,7 +59,7 @@ export default function TodoForm({ insertTodo }) {
     return (
             <form onSubmit={handleSubmit}>
                 <input type="text" value={currTodo.title} onChange={handleChange} />
-                <input type="submit" value="Do it"></input>
+                <Input type="submit" value="Do it"></Input>
             </form>
     )
 }
